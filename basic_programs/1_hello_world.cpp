e7b625e83c8ebef27cdd7061df4f87c84b1cda6f
Hina Qureshi <officialhinaqureshi@gmail.com>
2022-10-22 15:36:20 +0530

    Created Hello world simple C++ program shows Hello world on console

---
+++ basic_programs/1_hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// starting point of C++ Program that is main function
+int main()
+{
+    // insert Hello world into output stream that is console
+    std::cout << "Hello world" << std::endl;
+
+    // return 0 to the operating system mean main function executed successfully
+    return 0;
+}
